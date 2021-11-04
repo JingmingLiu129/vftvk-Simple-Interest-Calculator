72a9788148b977fc03ad653f75e6031b141d25c6
JingmingLiu129 <81575764+JingmingLiu129@users.noreply.github.com>
2021-11-04 21:30:58 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function compute()
     var p = document.getElementById("principal").value;
     var r = document.getElementById("rate").value;
     var y = document.getElementById("years").value;
-    getResult = function () {
-        let getresult = p*r*y/100
+    show = function () {
+        let show = p*r*y/100
     }；
     alert('If you deposit ' + p + ',');
     alert('at an interest rate of ' + r + '.');
-    alert('You will receive an amount of ' + getResult + ',');
+    alert('You will receive an amount of ' + show + ',');
     alert('in the year ' + r );
 }；
