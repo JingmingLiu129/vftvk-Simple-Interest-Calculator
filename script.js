e938e42f504fcb351811780e737b5abc558f455f
JingmingLiu129 <81575764+JingmingLiu129@users.noreply.github.com>
2021-11-04 18:32:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    getResult = function () {
+        let getresult = p*r*y
 }
-        
+    console.log('If you deposit ' + p + ',');
+    console.log('at an interest rate of ' + r + '.');
+    console.log('You will receive an amount of ' + getResult + ',');
+    console.log('in the year ' + r );
