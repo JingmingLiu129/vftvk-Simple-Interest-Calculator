4ccdd2731c31cec8b35b9fe0dbfc5f68e17a2742
JingmingLiu129 <81575764+JingmingLiu129@users.noreply.github.com>
2021-11-05 15:40:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var r = document.getElementById("rate").value;
-    var y = document.getElementById("years").value;
-    show = function () {
-        let show = p*r*y/100
-    }；
-    alert('If you deposit ' + p + ',');
-    alert('at an interest rate of ' + r + '.');
-    alert('You will receive an amount of ' + show + ',');
-    alert('in the year ' + r );
-}；
+    var put1 = document.getElementById("principal");
+    var rate1 = document.getElementById("rate");
+    var years1 = document.getElementById("years");
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    var div3 = document.getElementById("div3");
+    var btn1 = document.getElementById("btn");
+    var btn2 = document.getElementById("btn2");
+    put1.onkeyup = function() {
+        div1.innerText = put1.value;
+    }
+    rate1.onkeyup = function() {
+        div2.innerText = rate1.value;
+    }
+    years1.onkeyup = function() {
+            div3.innerText = years1.value;
+        }
+        //点击按钮,通过类样式的方式设置div的显示和隐藏
+        // function compute() {
+        //     console.log('1');
+        //     bon2.classList.remove("bon2")
+        // };
+    var flag = true
+    btn1.onclick = function() {
+        if (flag) {
+            bon2.style.display = 'none'
+            flag = false
+        } else {
+            flag = true
+            bon2.style.display = 'block'
+        }
+}
